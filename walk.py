ebf09bc9391c30edbc95e88b734d78ab82ce7fcd
Modupe Akanni <modupe775@gmail.com>
2023-04-24 14:55:44 +0100

    feat: add recursive function

--- walk.py
+++ walk.py
@@ -3,6 +3,10 @@ from os.path import isdir, join
 
 def walk_directory(d, indent):
     # Implement recursive directory walk here
+    for file in listdir(d):
+        print(indent + file)
+        if isdir(join(d,file)):
+            walk_directory(join(d, file), 2*indent)
 
 # '.' means current directory
 walk_directory('Desktop', ' ')
